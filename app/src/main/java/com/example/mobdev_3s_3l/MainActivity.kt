624f1769e3bcc1f3f9186618e9fe5545959c5b46
Not-Saint-Paul <145997767+Not-Saint-Paul@users.noreply.github.com>
2025-06-28 22:13:40 +0700

    Добавлены Layout'ы, TextView и Button, а также обработчик для кнопки

--- app/src/main/java/com/example/mobdev_3s_3l/MainActivity.kt
+++ app/src/main/java/com/example/mobdev_3s_3l/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mobdev_3s_3l
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,69 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button = findViewById<Button>(R.id.button)
+        val tllTextViewFirst = findViewById<TextView>(R.id.tll_text_view_first)
+        val tllTextViewSecond = findViewById<TextView>(R.id.tll_text_view_second)
+        val tllTextViewThird = findViewById<TextView>(R.id.tll_text_view_third)
+        val bllTextViewFirst = findViewById<TextView>(R.id.bll_text_view_first)
+        val bllTextViewSecond = findViewById<TextView>(R.id.bll_text_view_second)
+        val bllTextViewThird = findViewById<TextView>(R.id.bll_text_view_third)
+        val clTextViewLeft = findViewById<TextView>(R.id.cl_text_view_left)
+        val clTextViewRight = findViewById<TextView>(R.id.cl_text_view_right)
+        val clTextViewCenter = findViewById<TextView>(R.id.cl_text_view_center)
+
+
+
+        fun changeNumberTo(number: Int) {
+            when(number) {
+                1 -> {
+                    tllTextViewFirst.setText("1")
+                    tllTextViewSecond.setText("")
+                    tllTextViewThird.setText("")
+                    bllTextViewFirst.setText("1")
+                    bllTextViewSecond.setText("")
+                    bllTextViewThird.setText("")
+                    clTextViewLeft.setText("1")
+                    clTextViewRight.setText("")
+                    clTextViewCenter.setText("")
+                }
+                2 -> {
+                    tllTextViewFirst.setText("")
+                    tllTextViewSecond.setText("2")
+                    tllTextViewThird.setText("")
+                    bllTextViewFirst.setText("")
+                    bllTextViewSecond.setText("")
+                    bllTextViewThird.setText("2")
+                    clTextViewLeft.setText("")
+                    clTextViewRight.setText("2")
+                    clTextViewCenter.setText("")
+                }
+                3 -> {
+                    tllTextViewFirst.setText("")
+                    tllTextViewSecond.setText("")
+                    tllTextViewThird.setText("3")
+                    bllTextViewFirst.setText("")
+                    bllTextViewSecond.setText("")
+                    bllTextViewThird.setText("3")
+                    clTextViewLeft.setText("")
+                    clTextViewRight.setText("")
+                    clTextViewCenter.setText("3")
+                }
+            }
+        }
+
+        var counter = 1
+
+        button.setOnClickListener {
+            changeNumberTo(counter)
+            if (counter < 3) {
+                counter++
+            }
+            else {
+                counter = 1
+            }
         }
     }
 }
